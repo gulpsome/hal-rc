8c78f15447b764ce928eb26bd0c301a1f734f311
Orlin M Bozhinov <om@soundsapiens.com>
2015-04-29 14:02:08 +0300

    gulp-use the next (first) version of beverage

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('beverage')(require('gulp'), {
+  scripts: {include: {build: 'index.js'}},
+  buildWatch: ['index.coffee'],
+  test: {},
+  testWatch: ['index.js', 'test/*.coffee']
+})
+
+gulp.task('dev', 'DEVELOP', [
+  'build',
+  'build:watch',
+  'test:watch'
+])
